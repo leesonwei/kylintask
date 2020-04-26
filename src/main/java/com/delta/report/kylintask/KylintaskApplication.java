fad1b49daacf37cf7f986b94ae763b9dded8aeb9
anywhere <weilizong2014@hotmail.com>
2020-04-26 20:25:20 +0800

    first commit

---
+++ src/main/java/com/delta/report/kylintask/KylintaskApplication.java
@@ -0,0 +1,13 @@
+package com.delta.report.kylintask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KylintaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KylintaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/delta/report/kylintask/KylintaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delta.report.kylintask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KylintaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
